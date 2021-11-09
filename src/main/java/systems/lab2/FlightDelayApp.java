a9fc585f422488ff583ffd0bb045c08edfb052aa
alina-ysic <alina.ysic@gmail.com>
2021-11-10 02:45:21 +0300

    Scripted auto-commit on change (2021-11-10 02:45:21) by gitwatch.sh

--- src/main/java/systems/lab2/FlightDelayApp.java
+++ src/main/java/systems/lab2/FlightDelayApp.java
@@ -15,7 +15,7 @@ public class FlightDelayApp {
         job.setJarByClass(FlightDelayApp.class);
         job.setJobName("JoinJob sort");
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
-        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportMapper.class); 
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
         //job.setPartitionerClass( );
         //job.setGroupingComparatorClass( );
