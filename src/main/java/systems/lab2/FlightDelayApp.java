64ad411d5b341a0c2d1b0f3373becd372f13f918
alina-ysic <alina.ysic@gmail.com>
2021-11-10 02:44:00 +0300

    Scripted auto-commit on change (2021-11-10 02:44:00) by gitwatch.sh

---
+++ src/main/java/systems/lab2/FlightDelayApp.java
@@ -0,0 +1,29 @@
+package systems.lab2;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class FlightDelayApp {
+
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+        Job job = Job.getInstance();
+        job.setJarByClass(FlightDelayApp.class);
+        job.setJobName("JoinJob sort");
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportMapper.class); 
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        //job.setPartitionerClass( );
+        //job.setGroupingComparatorClass( );
+        //job.setReducerClass(JoinReducer.class);
+        //job.setMapOutputKeyClass(TextPair.class);
+        //job.setOutputKeyClass(Text.class);
+        //job.setOutputValueClass(Text.class);
+        job.setNumReduceTasks(2);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
--- src/main/java/systems/lab2/FlightMapper.java
+++ src/main/java/systems/lab2/FlightMapper.java
@@ -1,5 +1,7 @@
 package systems.lab2;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.io.Text;
 
-public class FlightMapper extends Mapper<> {
+public class FlightMapper extends Mapper<LongWritable, Text, , Text> {
 }
