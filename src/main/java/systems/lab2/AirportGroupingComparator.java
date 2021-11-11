4f319e37811b8a5f05fef30ba8ae2a485a00a65e
alina-ysic <alina.ysic@gmail.com>
2021-11-11 17:18:37 +0300

    Scripted auto-commit on change (2021-11-11 17:18:37) by gitwatch.sh

--- src/main/java/systems/lab2/AirportGroupingComparator.java
+++ src/main/java/systems/lab2/AirportGroupingComparator.java
@@ -3,5 +3,8 @@ package systems.lab2;
 import org.apache.hadoop.io.WritableComparator;
 
 public class AirportGroupingComparator extends WritableComparator {
-    
+
+    public AirportGroupingComparator() {
+        
+    }
 }
