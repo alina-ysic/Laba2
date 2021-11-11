ab2191a79938779540ac93e0ea740266e30dd735
alina-ysic <alina.ysic@gmail.com>
2021-11-11 17:30:52 +0300

    Scripted auto-commit on change (2021-11-11 17:30:52) by gitwatch.sh

--- src/main/java/systems/lab2/AirportReducer.java
+++ src/main/java/systems/lab2/AirportReducer.java
@@ -2,5 +2,12 @@ package systems.lab2;
 
 import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.IOException;
+
 public class AirportReducer extends Reducer{
+
+    @Override
+    protected void reduce(Object key, Iterable values, Context context) throws IOException, InterruptedException {
+        super.reduce(key, values, context);
+    }
 }
--- src/main/java/systems/lab2/FlightDelayApp.java
+++ src/main/java/systems/lab2/FlightDelayApp.java
@@ -1,6 +1,7 @@
 package systems.lab2;
 
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
@@ -21,8 +22,8 @@ public class FlightDelayApp {
         job.setGroupingComparatorClass(AirportGroupingComparator.class);
         job.setReducerClass(AirportReducer.class);
         job.setMapOutputKeyClass(AirportIdWritableComparable.class);
-        //job.setOutputKeyClass(Text.class);
-        //job.setOutputValueClass(Text.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
         job.setNumReduceTasks(2);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
