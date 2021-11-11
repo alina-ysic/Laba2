02bec6977357c12fc25fba21f2d00452977b5ba4
alina-ysic <alina.ysic@gmail.com>
2021-11-11 18:55:26 +0300

    Scripted auto-commit on change (2021-11-11 18:55:25) by gitwatch.sh

--- src/main/java/systems/lab2/FlightMapper.java
+++ src/main/java/systems/lab2/FlightMapper.java
@@ -6,6 +6,8 @@ import org.apache.hadoop.io.Text;
 import java.io.IOException;
 
 public class FlightMapper extends Mapper<LongWritable, Text, AirportIdWritableComparable, Text> {
+
+    private static final int INDICATOR = 1;
     @Override
     protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, AirportIdWritableComparable, Text>.Context context) throws IOException, InterruptedException {
         //super.map(key, value, context);
