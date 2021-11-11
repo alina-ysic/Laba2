acd59620112dab1a3f858e85b523951bfaa95f26
alina-ysic <alina.ysic@gmail.com>
2021-11-11 09:17:37 +0300

    Scripted auto-commit on change (2021-11-11 09:17:37) by gitwatch.sh

--- src/main/java/systems/lab2/AirportIdWritableComparable.java
+++ src/main/java/systems/lab2/AirportIdWritableComparable.java
@@ -8,7 +8,8 @@ import java.io.IOException;
 
 public class AirportIdWritableComparable extends WritableComparable {
 
-    private 
+    private int airportId;
+    private int ;
 
     @Override
     public int compareTo(Object o) {
