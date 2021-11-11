cacd62d0e8bc12e868fc46b8060ce4c834986202
alina-ysic <alina.ysic@gmail.com>
2021-11-11 15:00:35 +0300

    Scripted auto-commit on change (2021-11-11 15:00:35) by gitwatch.sh

--- src/main/java/systems/lab2/AirportPartitioner.java
+++ src/main/java/systems/lab2/AirportPartitioner.java
@@ -1,6 +1,11 @@
 package systems.lab2;
 
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Partitioner;
 
-public class AirportPartitioner extends Partitioner {
+public class AirportPartitioner extends Partitioner<AirportIdWritableComparable, Text> {
+    @Override
+    public int getPartition(AirportIdWritableComparable airportIdWritableComparable, Text text, int i) {
+        return 0;
+    }
 }
