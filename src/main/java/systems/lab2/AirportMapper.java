e6a125a1be11aae314d345776c110470cf780d80
alina-ysic <alina.ysic@gmail.com>
2021-11-11 10:01:49 +0300

    Scripted auto-commit on change (2021-11-11 10:01:49) by gitwatch.sh

--- src/main/java/systems/lab2/AirportMapper.java
+++ src/main/java/systems/lab2/AirportMapper.java
@@ -1,6 +1,13 @@
 package systems.lab2;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class AirportMapper extends Mapper<> {
+import java.io.IOException;
 
+public class AirportMapper extends Mapper<LongWritable, Text, AirportIdWritableComparable, Text> {
+    @Override
+    protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, AirportIdWritableComparable, Text>.Context context) throws IOException, InterruptedException {
+        super.map(key, value, context);
+    }
 }
--- src/main/java/systems/lab2/FlightMapper.java
+++ src/main/java/systems/lab2/FlightMapper.java
@@ -3,5 +3,11 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.io.Text;
 
+import java.io.IOException;
+
 public class FlightMapper extends Mapper<LongWritable, Text, AirportIdWritableComparable, Text> {
+    @Override
+    protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, AirportIdWritableComparable, Text>.Context context) throws IOException, InterruptedException {
+        super.map(key, value, context);
+    }
 }
