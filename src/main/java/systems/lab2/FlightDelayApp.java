976da2dfbd4a968dda03a9bea8ab2e53399f6d47
alina-ysic <alina.ysic@gmail.com>
2021-11-11 10:13:47 +0300

    Scripted auto-commit on change (2021-11-11 10:13:47) by gitwatch.sh

--- src/main/java/systems/lab2/FlightDelayApp.java
+++ src/main/java/systems/lab2/FlightDelayApp.java
@@ -21,7 +21,7 @@ public class FlightDelayApp {
         //job.setGroupingComparatorClass( );
         //job.setReducerClass(JoinReducer.class);
         //job.setMapOutputKeyClass(TextPair.class);
-        //job.setOutputKeyClass(Text.class);
+        job.setOutputKeyClass(Text.class);
         //job.setOutputValueClass(Text.class);
         job.setNumReduceTasks(2);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
