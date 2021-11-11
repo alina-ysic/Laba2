51b92326ed57e0544117f961fefe2809b124a28b
alina-ysic <alina.ysic@gmail.com>
2021-11-11 10:00:55 +0300

    Scripted auto-commit on change (2021-11-11 10:00:55) by gitwatch.sh

--- src/main/java/systems/lab2/FlightMapper.java
+++ src/main/java/systems/lab2/FlightMapper.java
@@ -3,5 +3,5 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.io.Text;
 
-public class FlightMapper extends Mapper<LongWritable, Text, , Text> {
+public class FlightMapper extends Mapper<LongWritable, Text, AirportIdWritableComparable, Text> {
 }
