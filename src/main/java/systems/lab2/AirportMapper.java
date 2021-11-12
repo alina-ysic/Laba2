30623301a3687a48abebc2611c80c6bb00d1f9d1
alina-ysic <alina.ysic@gmail.com>
2021-11-12 08:08:46 +0300

    Scripted auto-commit on change (2021-11-12 08:08:46) by gitwatch.sh

--- src/main/java/systems/lab2/AirportMapper.java
+++ src/main/java/systems/lab2/AirportMapper.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 public class AirportMapper extends Mapper<LongWritable, Text, AirportIdWritableComparable, Text> {
 
     private static final int INDICATOR = 0;
+    private static final 
     @Override
     protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, AirportIdWritableComparable, Text>.Context context) throws IOException, InterruptedException {
         //super.map(key, value, context);
