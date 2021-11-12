2b552abbfc01bbee037892f585408caba9e25eba
alina-ysic <alina.ysic@gmail.com>
2021-11-12 10:32:05 +0300

    Scripted auto-commit on change (2021-11-12 10:32:05) by gitwatch.sh

--- src/main/java/systems/lab2/AirportReducer.java
+++ src/main/java/systems/lab2/AirportReducer.java
@@ -10,7 +10,7 @@ public class AirportReducer extends Reducer<AirportIdWritableComparable, Text, T
 
     @Override
     protected void reduce(AirportIdWritableComparable key, Iterable<Text> values, Reducer<AirportIdWritableComparable, Text, Text, Text>.Context context) throws IOException, InterruptedException {
-        System.out.println(key.getAirportId());
+        Text airportId = key.getAirportId();
         Iterator<Text> iterator = values.iterator();
 
         while (iterator.hasNext()) {
