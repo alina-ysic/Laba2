b64a2ab8f70af2670a6d5d0e635699df347ebe92
alina-ysic <alina.ysic@gmail.com>
2021-11-12 10:36:13 +0300

    Scripted auto-commit on change (2021-11-12 10:36:13) by gitwatch.sh

--- src/main/java/systems/lab2/AirportReducer.java
+++ src/main/java/systems/lab2/AirportReducer.java
@@ -20,7 +20,7 @@ public class AirportReducer extends Reducer<AirportIdWritableComparable, Text, T
 
         while (iterator.hasNext()) {
             System.out.println(iterator.next());
-            int delay = Integer.parseInt(iterator.next());
+            float delay = Float.parseFloat(iterator.next());
             if (delay < min) min = delay;
             if (delay > max) max = delay;
             sum += delay;
