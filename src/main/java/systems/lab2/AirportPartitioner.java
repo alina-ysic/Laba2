ea78cda76c4196aeffd36055c4d314799c559cb7
alina-ysic <alina.ysic@gmail.com>
2021-11-12 09:33:59 +0300

    Scripted auto-commit on change (2021-11-12 09:33:59) by gitwatch.sh

--- src/main/java/systems/lab2/AirportPartitioner.java
+++ src/main/java/systems/lab2/AirportPartitioner.java
@@ -6,7 +6,7 @@ import org.apache.hadoop.mapreduce.Partitioner;
 public class AirportPartitioner extends Partitioner<AirportIdWritableComparable, Text> {
     @Override
     public int getPartition(AirportIdWritableComparable airportIdWritableComparable, Text text, int numTask) {
-        System.out.println("PART EEE");
+        //System.out.println("PART EEE");
         return airportIdWritableComparable.getAirportId() % numTask;
     }
 }
