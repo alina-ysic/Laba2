d15ab6619eb6770867b540f198148ed8dad18c73
alina-ysic <alina.ysic@gmail.com>
2021-11-12 08:19:55 +0300

    Scripted auto-commit on change (2021-11-12 08:19:55) by gitwatch.sh

--- src/main/java/systems/lab2/FlightMapper.java
+++ src/main/java/systems/lab2/FlightMapper.java
@@ -8,7 +8,8 @@ import java.io.IOException;
 public class FlightMapper extends Mapper<LongWritable, Text, AirportIdWritableComparable, Text> {
 
     private static final int INDICATOR = 1;
-    private static final String DELIMITER = "\",\"";
+    private static final String DELIMITER = ",";
+    private static final String DELIMITER = "\"";
     private static final int CODE_POS = 14;
     @Override
     protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, AirportIdWritableComparable, Text>.Context context) throws IOException, InterruptedException {
