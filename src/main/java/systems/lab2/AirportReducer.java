90dbb8f546c1e1d12da9176c5e4fa9e6465b1a5e
alina-ysic <alina.ysic@gmail.com>
2021-11-12 08:56:29 +0300

    Scripted auto-commit on change (2021-11-12 08:56:29) by gitwatch.sh

--- src/main/java/systems/lab2/AirportReducer.java
+++ src/main/java/systems/lab2/AirportReducer.java
@@ -4,12 +4,16 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 public class AirportReducer extends Reducer{
 
     @Override
     protected void reduce(Object key, Iterable values, Context context) throws IOException, InterruptedException {
         System.out.println(key);
-        Iterator<Text> iterator = values.iterator();
+
+        for (Text value : (Iterable<Text>) values) {
+            System.out.println(value);
+        }
     }
 }
